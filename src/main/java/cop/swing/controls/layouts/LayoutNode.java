8882077df4444f13864049b860b6f7eb71234a23
Oleg Cherednik <oleg.cherednik@xyleme.com>
2015-08-14 10:20:28 +0300

    Remove unused classes

--- src/main/java/cop/swing/controls/layouts/LayoutNode.java
+++ src/main/java/cop/swing/controls/layouts/LayoutNode.java
@@ -1,6 +1,6 @@
 package cop.swing.controls.layouts;
 
-import cop.swing.controls.Node;
+import cop.swing.panels.CreateFactory;
 
 import javax.swing.Box;
 import java.awt.Component;
@@ -9,47 +9,25 @@ import java.awt.Component;
  * @author Oleg Cherednik
  * @since 15.11.2012
  */
-public enum LayoutNode implements Node {
-    GLUE,
-    V_GLUE {
-        @Override
-        public Component create() {
-            return Box.createVerticalGlue();
-        }
-    },
-    H_GLUE {
-        @Override
-        public Component create() {
-            return Box.createHorizontalGlue();
-        }
-    };
+public enum LayoutNode implements CreateFactory<Component> {
+	GLUE,
+	V_GLUE {
+		@Override
+		public Component create() {
+			return Box.createVerticalGlue();
+		}
+	},
+	H_GLUE {
+		@Override
+		public Component create() {
+			return Box.createHorizontalGlue();
+		}
+	};
 
-    public String getText(Component obj) {
-        return null;
-    }
+	// ========== CreateFactory ==========
 
-    public void setText(Component obj, String text) {
-    }
-
-    public void doAction(Component obj) {
-    }
-
-    // ========== CreateFactory ==========
-
-    @Override
-    public Component create() {
-        return null;
-    }
-
-    // ========== KeyId ==========
-
-    @Override
-    public String getId() {
-        return null;
-    }
-
-    @Override
-    public boolean check(String id) {
-        return false;
-    }
+	@Override
+	public Component create() {
+		return null;
+	}
 }
